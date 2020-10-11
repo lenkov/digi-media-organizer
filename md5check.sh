06dd0413863fab426b4096a03759588124aa139c
Ivaylo Lenkov <lenkov@gmail.com>
2020-10-11 11:26:33 -0700

    moving IFS outside the loop

--- md5check.sh
+++ md5check.sh
@@ -2,15 +2,12 @@
 # Compare the md5 checksums of all files with the content of md5.txt and prints the difference
 # Only top-level folders have md5.txt
 
+IFS=$'\n'
 
 for i in `find * -type d -maxdepth 0`; do
     cd $i
-    IFS=$'\n'
     files=(`find * -type f | egrep -v "md5.txt|DS_Store"`) 
     echo "Scanning folder $i, files: ${#files[@]}"
     (cat md5.txt ; md5 ${files[*]}) | sort | uniq -c | grep -v "^   2 "
     cd ..
 done
-
-
-
